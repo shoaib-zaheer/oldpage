75f82c6ea4b2df9b446127210a68fe82b34021da
“Shoaib-zaheer” <z.shoaib015@gmail.com>
2020-05-23 10:11:20 +0200

    exhibition page changes

---
+++ js/exhibitionSlider.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('.next').on('click', function(){
+    let currentImg = $('.active');
+    let nextImg = currentImg.next();
+
+    if(nextImg.length){
+      currentImg.removeClass('active').css('z-index', -10);
+      nextImg.addClass('active').css('z-index', 10);
+    }
+  });
+
+  $('.prev').on('click', function(){
+    console.log('click');
+    let currentImg = $('.active')
+    let prevImg = currentImg.prev();
+
+    if (prevImg.length){
+      currentImg.removeClass('active').css('z-index', -10);
+      prevImg.addClass('active').css('z-index', 10);
+    }
+  });
+  setTimeout(read(2000));
+});
