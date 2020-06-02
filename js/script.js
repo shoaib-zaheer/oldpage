2bbe4a74410c7e2f68425c138722c6966fa8734e
“Shoaib-zaheer” <z.shoaib015@gmail.com>
2020-06-03 01:18:37 +0200

    update deviantART logo

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ const navbar = document.querySelector('.navbar');
 
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
-  if (last) last.classList.remove("menuIcon, #drop-itDown");
-  
 });
 
 
@@ -13,5 +11,17 @@ const dropDown = document.querySelector('.dropdown-content');
 
 galleryIcon.addEventListener("click", () => {
   dropDown.classList.toggle("newOne");
-
 });
+
+
+
+$('#mainmenu a').click(function(event){
+  event.preventDefault();
+  event.stopPropagation();
+  var subtopic = $(this).parent().find('.subtopics');
+  $('.subtopics').not(subtopic).hide();
+  subtopic.show();
+})
+$(document).click(function(event){
+  $('.subtopics').hide();
+})
